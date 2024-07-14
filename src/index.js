6336cf42a0650c4f8300e2cdcb43c51649a155f6
Sheral123 <sheralsingla18@gmail.com>
2024-07-15 01:16:58 +0530

    added  notification service

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const{ ServerConfig, Logger } = require('./config');
-
+const amqplib = require('amqplib');
+const{EmailService} = require('./services')
 const express = require('express');
 
+async function connectQueue(){
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", async(data) => {
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail("airlinenoti023@gmail.com", object.recepientEmail, object.subject, object.text)
+        })
+
+    } catch (error) {
+       // console.log(error);
+    }
+}
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -12,7 +27,9 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.group(`Succesfully started the server on PORT : ${ServerConfig.PORT}`);
     //Logger.info("succesfully started the server", {});
+    await connectQueue();
+    console.log("queue is up")
 });
